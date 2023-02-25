feba3e56df953783c9620c68fe20f89238f8276f
amindWalker <bhrochamail@gmail.com>
2023-02-25 20:54:10 -0300

    `docs:` update README
    `refactor:` move assets location

--- app/src/api_service/marvel.rs
+++ app/src/api_service/marvel.rs
@@ -2,7 +2,7 @@
 use dioxus::prelude::*;
 use fermi::{use_atom_state, use_read, use_set};
 // Local depencendies
-use crate::{types::{CharactersRoot}, LOADING, PAGE_LIMIT, ROOT_API};
+use crate::{types::CharactersRoot, LOADING, PAGE_LIMIT, ROOT_API};
 
 const MARVEL_BASE_URL: &str = env!("MARVEL_BASE_URL");
 const MARVEL_PBK: &str = env!("MARVEL_PBK");
@@ -14,31 +14,31 @@ pub struct PageLimit(pub usize);
 pub struct PageOffset(pub usize);
 
 // #[cached::proc_macro::once(time = 86400, option = true, sync_writes = true)]
-// pub(crate) fn fetch_and_cache(cx: Scope) -> Option<CharactersRoot> {
-//     let limit = *use_read(cx, PAGE_LIMIT);
-//     let offset = "";
-//     log::info!("[FETCH SERVICE] LIMIT: {limit}");
-//     let set_loading = use_atom_state(cx, LOADING);
+pub(crate) fn fetch_and_cache(cx: Scope) -> Option<CharactersRoot> {
+    let limit = *use_read(cx, PAGE_LIMIT);
+    let offset = "";
+    println!("[FETCH SERVICE] LIMIT: {limit}");
+    let set_loading = use_atom_state(cx, LOADING);
 
-//     let fetch = use_future(cx, &(limit, offset), |(limit, offset)| async move {
-//         // gloo::net::http::Request::get(&format!("{MARVEL_BASE_URL}/v1/public/characters?limit={limit}&offset={offset}&ts=9&apikey={MARVEL_PBK}&hash={MARVEL_API_HASH}"))
-//         gloo::net::http::Request::get("http://192.168.1.131:5500/mock.json")
-//             .send()
-//             .await
-//             .unwrap()
-//             .json::<CharactersRoot>()
-//             .await
-//     });
+    let fetch = use_future(cx, &(limit, offset), |(limit, offset)| async move {
+        // gloo::net::http::Request::get(&format!("{MARVEL_BASE_URL}/v1/public/characters?limit={limit}&offset={offset}&ts=9&apikey={MARVEL_PBK}&hash={MARVEL_API_HASH}"))
+        gloo::net::http::Request::get("http://192.168.1.131:5500/mock.json")
+            .send()
+            .await
+            .unwrap()
+            .json::<CharactersRoot>()
+            .await
+    });
 
-//     match fetch.value() {
-//         Some(data) => {
-//             set_loading.set(false);
-//             global_state(cx, data.as_ref().ok().cloned());
-//             data.as_ref().ok().cloned()
-//         }
-//         _ => None,
-//     }
-// }
+    match fetch.value() {
+        Some(data) => {
+            set_loading.set(false);
+            global_state(cx, data.as_ref().ok().cloned());
+            data.as_ref().ok().cloned()
+        }
+        _ => None,
+    }
+}
 
 pub(crate) fn global_state(cx: &ScopeState, cache: Option<CharactersRoot>) {
     if let Some(cached_data) = cache.as_ref().cloned() {
--- app/src/main.rs
+++ app/src/main.rs
@@ -1,9 +1,6 @@
 #![allow(non_snake_case)]
 #![feature(is_some_and)]
-use std::net::SocketAddr;
 
-use axum::{routing::get, Router};
-use axum_extra::routing::SpaRouter;
 // External depencendies
 use components::{Home, Nav, NavBar};
 use dioxus::prelude::*;
@@ -14,68 +11,29 @@ use types::CharactersRoot;
 mod api_service;
 mod components;
 mod types;
-use crate::components::{Footer, Header};
+use crate::{
+    api_service::fetch_and_cache,
+    components::{Footer, Header},
+};
 
 pub(crate) static ROOT_API: Atom<Option<CharactersRoot>> = |_| None;
 pub(crate) static NAV_BAR: Atom<NavBar> = |_| NavBar(false);
 pub(crate) static PAGE_LIMIT: Atom<usize> = |_| 100;
 pub(crate) static LOADING: Atom<bool> = |_| true;
 
-#[tokio::main]
-async fn main() {
-    // wasm_log::init(wasm_log::Config::default());
-    // dioxus_web::launch(App)
+const MARVEL_BASE_URL: &str = env!("MARVEL_BASE_URL");
+const MARVEL_PBK: &str = env!("MARVEL_PBK");
+const MARVEL_API_HASH: &str = env!("MARVEL_API_HASH");
 
-    let (SOCKET, PORT) = ([127, 0, 0, 1], 8080);
-    let addr = SocketAddr::from((SOCKET, PORT));
-    let view = dioxus_liveview::LiveViewPool::new();
-
-    let app = Router::new()
-        .route(
-            "/",
-            axum::routing::get(move || async move {
-                axum::response::Html(format!(
-                    r#"
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <title>Marvel Collection</title>
-                <meta content="text/html;charset=utf-8" http-equiv="Content-Type" />
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <meta charset="UTF-8" />
-                <link rel="stylesheet" href="assets/uno.css">
-                <link rel="stylesheet" href="assets/tailwind.min.css">
-            </head>
-            <body> <div id="main"></div> </body>
-                {glue}
-            </html>
-            "#,
-                    glue = dioxus_liveview::interpreter_glue(&format!("ws://{addr}/ws"))
-                ))
-            }),
-        )
-        .route(
-            "/ws",
-            get(move |ws: axum::extract::WebSocketUpgrade| async move {
-                ws.on_upgrade(move |socket| async move {
-                    _ = view.launch(dioxus_liveview::axum_socket(socket), App).await;
-                })
-            }),
-        )
-        .merge(SpaRouter::new("/assets", "assets").index_file("index.html"));
-
-    println!("Listening on http://{addr}");
-
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
-
+fn main() {
+    wasm_log::init(wasm_log::Config::default());
+    dioxus_web::launch(App)
 }
 
 fn App(cx: Scope) -> Element {
     use_init_atom_root(cx);
 
+    println!("{:?}", fetch_and_cache(cx));
     cx.render(rsx! {
         Router {
             Header {}
